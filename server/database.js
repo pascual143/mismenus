11afd4d6593db514b66c999f8d26640a38ad1e87
Pascual <pascual.vila.segura@hotmail.com>
2020-03-15 12:00:31 +0000

    nuevo empezar

--- server/database.js
+++
@@ -1,9 +0,0 @@
-//adding mongoose to this file
-const mongoose = require('mongoose')
-
-//Initiating a connection to the database
-// mongoose.connect('mongodb://chris:chris22@ds211774.mlab.com:11774/recipesdatabase', { useNewUrlParser: true }); 
-mongoose.connect('mongodb://localhost:27017');
-
-//Connecting to the database
-const connect = mongoose.connection;
